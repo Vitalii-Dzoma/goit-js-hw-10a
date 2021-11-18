de8c41a482377d3ef6a26e560769df5e1dd51e6c
Vitalii-Dzoma <vitoos900@gmail.com>
2021-11-18 15:21:07 +0200

    исправлена ДЗ 18.11.2021

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 import './css/styles.css';
-import "./fetchCountries.js"
-import { fetchExport } from './fetchCountries.js';
 import { debounce } from 'lodash';
-
-export const galleryUl = document.querySelector('.country-list');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import API from "./fetchCountries.js"
+const galleryUl = document.querySelector('.country-list');
 const DEBOUNCE_DELAY = 300;
-    const inputData = document.querySelector("#search-box");
+const inputData = document.querySelector("#search-box");
 
 
 
 
 function onSearch(e) {
-console.log(inputData.value)
-  inputData.value.trim()
-fetchExport(inputData.value)
+    const searchQuery = inputData.value;
+    if (!searchQuery.trim()) {
+            return;
+        }
+    API.fetchExport(searchQuery)
+    .then(data => {
+        console.log(data.length)
+        
+            if (data.length > 10) {
+            
+               Notify.info("Too many matches found. Please enter a more specific name.")
+               return option = '';
 
-    if (!inputData.value) {
-        return;
-    }
-  
+            }
+            if (data.length < 10 && data.length > 1) {
+               return data.map(({ capital, name, population, languages, flags, }) =>
+                  `<img src="${flags.svg}" alt="${name.official}" class="county-flag" width="30px">
+                <p class="country-name">${name.official}</p>`
+               ).join('')
+            }
+            return data.map(({ capital, name, population, languages, flags, }) =>
+               `<div class="county-items">
+       <div class="img-container">
+            <img src="${flags.svg}" alt="${name.official}" class="county-flag" width="30px">
+                <p class="country-name">${name.official}</p>
+            <p>
+                <span class="country-population">Population:${population}</span>
+                    <p class="country-lanquages">Languages:${Object.values(languages)}</p>
+                        <p class="country-capital">Capital:${[...capital]}</p>
+     
+            </p>
+        </div>
+    </div>`
+               
+      
+            ).join('')
+    })
+     .then((resp) => { galleryUl.innerHTML = resp })
+        .catch(err => { galleryUl.innerHTML = '', Notify.failure("Oops, there is no country with that name"); return })
+
 
 }
 
 
-
-// function moreThenTenCountries() {
-//     if ()
-// }
   
 inputData.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY))
